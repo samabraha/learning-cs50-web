1bf05d6dadd2f7b4e004e6931c27ba677bdc13cc
samabraha <samson.abraha@gmail.com>
2021-10-25 14:26:52 +0100

    Continued building the tasks app

--- MY_PROJECT/tasks/views.py
+++ MY_PROJECT/tasks/views.py
@@ -4,5 +4,9 @@ tasks = ['foo', 'bar', 'baz']
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
-        
-    } )
+        "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")
+
